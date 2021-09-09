84eb3861dec45217d84c827cf0352e013a2c6869
Tinson Lai <laitingsheng@hotmail.com>
2021-09-09 11:31:32 +0930

    Add aliases for ls

--- .main.sh
+++ .main.sh
@@ -1,4 +1,8 @@
-function _wrap_gnu() {
+function _dircolors() {
+    [ -r ~/.dircolors ] && eval "$($1 -b ~/.dircolors)" || eval "$($1 -b)"
+}
+
+function _wrap_colour() {
     prefix=$1
     shift 1
 
@@ -9,21 +13,33 @@ function _wrap_gnu() {
     done
 }
 
+function _ls_aliases() {
+    prefix=${1:-}
+    ls="${prefix}ls"
+    alias "${prefix}l"="${ls} -CF"
+    alias "${prefix}la"="${ls} -AF"
+    alias "${prefix}ll"="${ls} -ahlF"
+}
+
 case $OSTYPE in
     darwin*)
         export CLICOLOR=1
 
         alias ls='ls -G'
+        _wrap_colour '' grep egrep fgrep
+        _ls_aliases
 
-        _wrap_gnu g ls dir vdir grep egrep fgrep
+        _dircolors "$(brew --prefix)/bin/gdircolors"
+        _wrap_colour g ls dir vdir grep egrep fgrep
+        _ls_aliases g
         ;;
     linux*)
-        _wrap_gnu '' ls dir vdir
+        _dircolors '/usr/bin/dircolors'
+        _wrap_colour '' ls dir vdir grep egrep fgrep
+        _ls_aliases
         ;;
     *)
         echo "system-utils: cannot recognise the OS type (${OSTYPE:-unknown})" >&2
-        echo "system-utils: nothing has been modified" >&2
+        echo 'system-utils: nothing has been modified' >&2
         ;;
 esac
-
-_wrap_gnu '' grep egrep fgrep
